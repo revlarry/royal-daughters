c95d88cdcb0333715fcd5c5a83980c4076222829
Larry Dorkenoo <dorkenool@msn.com>
2020-07-18 17:05:59 +0200

    My 1st commit on this project

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,226 @@
+@extends('layouts.layout')
+
+
+@section('content')
+
+<div id="breadcrumb">
+    <div class="container">
+        <div class="breadcrumb">
+          <li><a href="index.html">Home</a></li>
+          <li>About Us</li>
+        </div>
+    </div>
+</div>
+
+<div class="aboutus">
+  
+    <div class="about">
+    <div class="container">
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <h2>About us</h2>
+        <img src="/images/leaders/revhelen.png" class="img-responsive" />
+        <h3>Rev. HelenRuth Dorkenoo, Founder / President</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero,
+          pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+        </p>
+      </div>
+
+      <div class="col-md-8 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <h2>The Vision</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero,
+          pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+            libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
+            libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="our-team">
+    <div class="container">
+      <h3>Leadership - Netherlands</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="/images/leaders/royal_sec.jpg" alt="">
+          
+          <h4>Josephine Bonney</h4>
+          <h5>Royal Secretary</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <img src="/images/leaders/royal_pro.jpg" alt="">
+          <h4>Pearl Kunst</h4>
+          <h5>Royal Public Relations Officer</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="/images/leaders/royal_welfare_officer.jpg" width="300px" height="300px" alt="">
+          <h4>Grace Omani</h4>
+          <h5>Royal Welfare Officer</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="our-team">
+    <div class="container">
+      <!-- <h3>Leadership</h3> -->
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="/images/leaders/royal_coordinator.jpg" alt="">
+          
+          <h4>Rejoice Doste</h4>
+          <h5>Royal Coordinator</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <img src="/images/leaders/royal_matron.jpg" alt="">
+          <h4>Felicia Ekeocha</h4>
+          <h5>Royal Matron</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="/images/leaders/royal_advisor.jpg" width="300px" height="300px" alt="">
+          <h4>Cecilia Achonu</h4>
+          <h5>Royal Adviser</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="our-team">
+    <div class="container">
+      <h3>Leadership - Ghana</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <!-- <img src="https://via.placeholder.com/300x300" alt=""> -->
+          <img src="/images/leaders/royal_coordinator_gh.jpg" alt="">
+          
+          <h4>Evelyne Avedzi</h4>
+          <h5>Royal Coordinator</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <img src="/images/leaders/royal_sec_gh.jpg" alt="">
+          <h4>Hannah Aryee</h4>
+          <h5>Royal Secretary</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="/images/leaders/royal_welfare_officer_gh.jpg" width="300px" height="300px" alt="">
+          <h4>Victor Ladzekpo</h4>
+          <h5>Royal Welfare Officer / Advisor</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="our-team">
+    <div class="container">
+      <!-- <h3>Leadership - Ghana</h3> -->
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <!-- <img src="https://via.placeholder.com/300x300" alt=""> -->
+          <img src="/images/leaders/royal_social_sec_gh.jpg" alt="">
+          
+          <h4>Rita Dorkenoo</h4>
+          <h5>Royal Social Secretary</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <!-- <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <img src="/images/leaders/royal_sec_gh.jpg" alt="">
+          <h4>Hannah Aryee</h4>
+          <h5>Royal Secretary</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div> -->
+        <!-- <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="/images/leaders/royal_welfare_officer_gh.jpg" width="300px" height="300px" alt="">
+          <h4>Victor Ladzekpo</h4>
+          <h5>Royal Welfare Officer</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div> -->
+      </div>
+    </div>
+</div>
+
+<div class="our-team">
+    <div class="container">
+      <h3>Royal Ambassadors</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="/images/leaders/royal_ambassador1.jpg" alt="">
+          <h4>Zahra Naleie | FSAN Nederland</h4>
+          <h5>Royal Ambassador</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <img src="/images/leaders/royal_ambassador2.jpg" alt="">
+          <h4>Mildred Kleinbussink | Allround Shipping</h4>
+          <h5>Royal Ambassador</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="/images/leaders/royal_ambassador3.jpg" alt="">
+          <h4>Dr. Pieternel Exter | Rtd. Amsterdam Ambulance Mgr</h4>
+          <h5>Royal Ambassador</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+<div class="our-team">
+    <div class="container">
+      <h3>Royal Mothers</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="/images/leaders/royal_mother1.jpg" alt="">
+          <h4>Rev. Mrs.Rita Korankye-Ankrah | Premier Lady, RCI Worldwide</h4>
+          <h5>Royal Mother</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <img src="/images/leaders/royal_mother2.jpg" alt="">
+          <h4>Archbishop Susuana Amarteifio | Prelate, Jesus Christ of Nazareth Intl.</h4>
+          <h5>Royal Mother</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <!-- <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="https://via.placeholder.com/300x300" alt="">
+          <h4>John Doe</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div> -->
+      </div>
+    </div>
+</div>
+
+<!-- div class="our-team">
+    <div class="container">
+      <h3>Royal Mothers</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="https://via.placeholder.com/300x300" alt="">
+          <h4>John Doe</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <img src="https://via.placeholder.com/300x300" alt="">
+          <h4>John Doe</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="https://via.placeholder.com/300x300" alt="">
+          <h4>John Doe</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+</div> -->
+@endsection
---
+++ resources/views/activities.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div id="breadcrumb">
+    <div class="container">
+      <div class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li>Activities</li>
+      </div>
+    </div>
+  </div>
+
+  <div class="services">
+    <div class="container">
+      <h3>Activities</h3>
+      <hr>
+      <div class="col-md-6">
+
+        <img src="images/activities/4.jpg" class="img-responsive">
+        <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero,
+          pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque</p> -->
+      </div>
+
+      <div class="col-md-6">
+
+        <div class="media">
+          <ul>
+            <li>
+              <div class="media-left">
+                <i class="fa fa-pencil"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Ladies' Sexual Health with Juan Walter</h4>
+                <p>Juan is a municipal health officer for the GGD in Amsterdam. He is specialised in giving seminars on health matters to both locals and migrant groups in Amsterdam. This training was intended to equip the Royal Daughters essential health information pertinent to women.</p>
+                <img src="images/activities/5.jpg" class="img-responsive">
+              </div>
+            </li>
+            <!-- <li>
+              <div class="media-left">
+                <i class="fa fa-book"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Responsive Design</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget.</p>
+              </div>
+            </li>
+            <li>
+              <div class="media-left">
+                <i class="fa fa-rocket"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Bootstrap Themes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget.</p>
+              </div>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sub-services">
+    <div class="container">
+      <div class="col-md-6">
+        <div class="media">
+          <ul>
+            <li>
+              <div class="media-left">
+                <i class="fa fa-pencil"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Financial Empowerment with Rita Menig</h4>
+                <p>A financial empowerment session was held with Ms. Rita Menig, an acclaimed coach in finance and economic matters. The Royal Daughters received tutorials on basic but vital habits and practices which can change a person's financial fortunes.</p>
+                <img src="images/activities/6.jpg" class="img-responsive">
+              </div>
+            </li>
+            <!-- <li>
+              <div class="media-left">
+                <i class="fa fa-book"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Training</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget.</p>
+              </div>
+            </li>
+            <li>
+              <div class="media-left">
+                <i class="fa fa-rocket"></i>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Logo Design</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget.</p>
+              </div>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <img src="images/activities/6.jpg" class="img-responsive">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque libero,
+          pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque</p>
+      </div>
+    </div>
+  </div>
+
+@endsection
